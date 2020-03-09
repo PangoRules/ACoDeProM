c3a11afc5bab4f3e9c7105aaf50ae54debb59929
PangoNiggas <animasdelmundo2@gmail.com>
2020-03-08 22:07:30 -0600

    Pagina de contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -8,11 +8,11 @@
 	<div class="container mt-5">
 		<h2 class="display-4 text-center" style="margin-top:40px!important;">CONTACTO</h2>
 		<div class="container text-center mt-5" style="margin-top:80px!important;">
-			<h4 class="display-5">Teléfono para Ventas</h4>
+			<h4 class="display-4" style="font-size: 2rem;"><b>Teléfono para Ventas</b></h4>
 			<p class="lead"><b>961-294-2885</b></p>
 		</div>
 		<div class="container text-center mt-5" style="margin-top:80px!important;">
-			<h4 class="display-4">Correo Electrónico</h4>
+			<h4 class="display-4" style="font-size: 2rem;"><b>Correo Electrónico</b></h4>
 			<p class="lead"><b>molinahdez67@hotmail.com</b></p>
 			<p class="lead"><b>molinahdez67@gmail.com</b></p>
 		</div>
